6c63f6565cc5ada59bf2950fed65533297751ccd
Luks17 <lucasthidavort@gmail.com>
2024-09-17 20:21:16 -0400

    feat(gen): typeorm col options

--- src/gen/database/models/mod.rs
+++ src/gen/database/models/mod.rs
@@ -2,6 +2,42 @@ pub mod user;
 
 use crate::core::react::{self, React};
 use genco::{quote_fn, tokens::FormatInto};
+use serde::Serialize;
+use serde_valid::{json::ToJsonString, Validate};
+use std::fmt::Display;
+
+#[derive(Serialize, Validate)]
+struct TypeOrmColumnOptions {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "type")]
+    type_: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    unique: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    length: Option<u16>,
+}
+
+impl TypeOrmColumnOptions {
+    pub fn new() -> Self {
+        TypeOrmColumnOptions {
+            type_: None,
+            unique: None,
+            length: None,
+        }
+    }
+}
+
+impl Display for TypeOrmColumnOptions {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let result = self.to_json_string();
+
+        if let Ok(json) = result {
+            return write!(f, "{}", json);
+        }
+
+        write!(f, "{{}}")
+    }
+}
 
 fn timestamps_cols() -> impl FormatInto<React> {
     let create_date_column = react::import("typeorm", "CreateDateColumn");
